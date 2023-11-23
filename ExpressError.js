c1b2ea45ba6822aeb2e954333822335c8b52ae38
haseeb hummo <haseebpersonalwork@gmail.com>
2023-11-23 11:45:09 +0530

    Error handling.

--- ExpressError.js
+++
@@ -1,8 +0,0 @@
-class ExpressError extends Error {
-    constructor(status , message){
-        super(message);
-        this.status = status;
-    }
-}
-
-module.exports = ExpressError;
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./ExpressError");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+const { listingSchema } = require("./schema");
 
 // Setting up the view engine and middleware
 app.set("view engine", "ejs");
@@ -31,10 +33,10 @@ app.get("/", (req, res) => {
 });
 
 // Display all listings
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index", { allListings });
-});
+}));
 
 // Display form to create a new listing
 app.get("/listings/new", (req, res) => {
@@ -42,62 +44,54 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Create a new listing
-app.post("/listings", async (req, res) => {
-    const newData = req.body;
-    const newListing = new Listing(newData);
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    const result = listingSchema.validate(req.body);
+    console.log(result);
+    const newListing = new Listing(req.body.listing);
+    console.log(newListing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 // Display form to edit a listing
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const data = await Listing.findById(id);
     res.render("listings/edit", { data });
-});
+}));
 
 // Update a listing
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const newData = await Listing.findByIdAndUpdate(id, req.body, { new: true });
     // console.log(newData);
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Display a specific listing
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/show", { listing });
-});
+}));
 
 // Delete a listing
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
-const checkToken =  (req , res , next) => {
-    const {token} = req.query;
-    if (token === "giveAccess") {                  
-        next();
-    }
-    else {
-        throw new ExpressError(401 , "Access Denied...");
-    }
-}
-
-app.get("/api" , checkToken ,  (req ,res) => {
-    abcd = abcd;
-    res.send("DATA processing...");
+app.all("*" , (req , res , next) => {
+    next(new ExpressError(404 , "No Page Found..."));
 })
 
-app.use( (err , req , res , next) => {
-    const {status = 500, message = "Kuch bhi default message likh sakte hain..."} = err;
-    console.log(status , message);
-    res.status(status).send(message);
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500 , message = "Somthing went wrong..."} = err;
+    console.error(err + " This is the error handling middleware...")
+    res.status(statusCode).render("listings/error" , {message});
 })
 
 // Start the server
